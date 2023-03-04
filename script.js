7007ecabc67064a02a096f4fcab8582c4207556e
renzobuendia <renzobuendia@gmail.com>
2023-03-04 17:17:22 -0500

    Leyendo HTML desde JavaScript

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("Hello, World");
+const h1 = document.querySelector('h1');
+const p = document.querySelectorAll('p');
+const parrafito = document.getElementsByClassName('parrafito');
+const pid = document.getElementById('pid');
+const input = document.querySelector('input');
+
+console.log(input.value);
+
+console.log({h1,
+    p,
+    parrafito,
+    pid,
+    input});
