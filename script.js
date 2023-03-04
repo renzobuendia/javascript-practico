cd6a57d074b0c9ee5170575c71df9c01bbd606ef
renzobuendia <renzobuendia@gmail.com>
2023-03-04 17:50:35 -0500

    Escribiendo HTML desde JavaScript

--- script.js
+++ script.js
@@ -11,3 +11,23 @@ console.log({h1,
     parrafito,
     pid,
     input});
+
+h1.innerText = 'Patito <br> Feo'
+console.log(h1.getAttribute('class'));
+h1.setAttribute('class', 'rojo');
+h1.classList.add('verde');
+h1.classList.remove('verde');
+// h1.classList.toggle('verde');
+// h1.classList.contains('verde');
+
+input.value = '456';
+
+const img = document.createElement('img');
+
+//img.src
+img.setAttribute('src', 'https://images.unsplash.com/photo-1677629322483-e9b7c49ee48f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1288&q=80');
+
+
+pid.innerHTML = "";
+pid.append(img);
+// pid.remove();
